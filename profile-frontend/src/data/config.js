46ae87ff08aa9347fd5249260ff13ab7464d75d3
Anant Shukla <anant.shukla16@gmail.com>
2020-12-06 11:19:57 +0530

    Change details

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,9 +1,9 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Anas Khan",
-  greeting: "Hey 👋",
-  greetingDescription: "I'm Anas Khan and I'm a Software Engineer!",
-  skillsDescription: "I'm a passionate software engineer focused on bringing quality to any product",
-  githubUrl: "https://github.com/anaskhan96",
+  name: "Anant Shukla",
+  greeting: "Hello!",
+  greetingDescription: "I'm Anant Shukla and I'm a Software Engineer!",
+  skillsDescription: "Agile and Versatile, I'm involved in fields like Data Science, AI and Full Stack Development, but also do video/audio work, game development, 3D modelling and technical writing on the side. ",
+  githubUrl: "https://github.com/Keepingshtum",
 };
